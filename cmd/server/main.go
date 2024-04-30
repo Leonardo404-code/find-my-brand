409c591944e62c4e058632bfd736799f8dc3d5ce
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-04-30 16:47:23 -0300

    docs: added documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 )
 
+// @contact.name Leonardo de Farias Bispo
+// @contact.email leonardobispo.dev@gmail.com
+// @Title Find my brand
+// @version 1.0
+// @description Find my brand documentation
 func main() {
 	r := http.NewServeMux()
 
