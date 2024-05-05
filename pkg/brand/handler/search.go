7ed69673cd183f561dbf4dee787c99f44b93937f
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-05-05 12:16:50 -0300

    fix: handled data error not found

--- pkg/brand/handler/search.go
+++ pkg/brand/handler/search.go
@@ -19,6 +19,8 @@ import (
 // @Produce json
 // @Success 200 {object} brand.Result
 func (h *handler) Search(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		customErr.Error(w, http.StatusInternalServerError, err)
@@ -36,10 +38,18 @@ func (h *handler) Search(w http.ResponseWriter, r *http.Request) {
 	}
 
 	location := r.URL.Query().Get("location")
+	if location == "" {
+		location = "Brazil"
+	}
 
 	result, err := h.brandSvc.SearchTerms(findBrandReq, location)
 	if err != nil {
 		customErr.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if len(result.Ads) == 0 {
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
